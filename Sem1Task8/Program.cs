4695038735014a78ca4795d8178858f187d67547
rafvlg <121390991+rafvlg@users.noreply.github.com>
2023-01-20 02:37:20 +0300

    SeminarvsDZ

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+string? inputNum = Console.ReadLine();
+if(inputNum!=null)
+{
+    int inputNumber1 = int.Parse(inputNum);
+    string result = string.Empty;
+    for(int i = 2; i < inputNumber1; i = i+2)
+    {
+        result = result + i + ",";
+    }
+    if(inputNumber1%2 == 0)
+    {
+        result = result + inputNumber1;
+    }
+    Console.WriteLine(result);
+}
---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №14
+// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+// Console.Write("Введите число:");
+// int num1 = int.Parse(Console.ReadLine() ?? "0");
+// int res1 = num1 % 7;
+// int res2 = num1 % 23;
+// if ((res1 == 0) && (res2 ==0 ))
+// {
+//     Console.Write("Число кратно 7 и 23");
+// }
+// else
+// {
+//     Console.Write("Число некратно 7 и 23");
+// }
+
+//Вариант 2
+
+int num = int.Parse(Console.ReadLine() ?? "0");
+if ((num%7==0)&&(num%23==0)){
+    Console.WriteLine("кратное");
+}else{
+    Console.WriteLine("не кратное 7и и 23ём");
+}
