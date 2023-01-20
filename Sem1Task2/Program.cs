bd6a0e169180c283aab29d2d78831e7a1b698a0a
rafvlg <121390991+rafvlg@users.noreply.github.com>
2023-01-20 17:17:54 +0300

    SeminarvsDZ

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+string? inputNum1 = Console.ReadLine();
+string? inputNum2 = Console.ReadLine();
+
+int A = int.Parse(inputNum1);
+int B = int.Parse(inputNum2);
+
+if(A > B)
+{
+    Console.WriteLine("число A больше B");
+}
+else
+{
+    Console.WriteLine("число B больше A");
+}
+
+
+
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+string? inputNum1 = Console.ReadLine();
+string? inputNum2 = Console.ReadLine();
+string? inputNum3 = Console.ReadLine();
+
+int A = int.Parse(inputNum1);
+int B = int.Parse(inputNum2);
+int C = int.Parse(inputNum3);
+
+if (A > B)
+{
+    if(A > C)
+    {
+        Console.WriteLine("Из заданных чисел A больше");
+    }
+    else
+    {
+        Console.WriteLine("Из заданных чисел C больше");
+    }
+}
+else
+{
+    if(B > C)
+    {
+            Console.WriteLine("Из заданных чисел B больше");
+        }
+        else
+        {
+            Console.WriteLine("Из заданных чисел C больше");
+    }
+}
