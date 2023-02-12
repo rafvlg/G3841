089ac9e00d055bc0cd511ae13c270bbe4672bf8a
rafvlg <121390991+rafvlg@users.noreply.github.com>
2023-02-12 23:16:32 +0300

    SeminarvsDZ

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+// Функция рекурсии
+int RecSumMN(int m, int n)
+{
+    if (m>=n)
+    {
+        return n;
+    }
+    else
+    {
+        
+        return m + RecSumMN(m+1, n);
+    }
+}
+
+//Вывод результата
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+//int sum = RecSumMN(m,n);
+int sum = m<n? RecSumMN(m, n):RecSumMN(n, m);
+PrintResult("Сумма M и N равна: ",sum);
+
+
+
+
+
+
+
+
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+//Функция Акермана
+int Acerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Acerman(n - 1, 1);
+    else
+      return Acerman(n - 1, Acerman(n, m - 1));
+}
+
+
+//Вывод результата
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+int sum = Acerman(m,n);
+PrintResult("Значение функции Аккермана равно: ",sum);
