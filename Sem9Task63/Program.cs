ad6907a616f2b04c7d312d0f0d03c96869631c60
rafvlg <121390991+rafvlg@users.noreply.github.com>
2023-02-12 22:26:25 +0300

    SeminarvsDZ

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+string LineGenRec(int num)
+{
+    if (num == 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        return LineGenRec(num-1)+" "+num;
+    }
+}
+
+
+int number = ReadData("Введите число N: ");
+string resultLine = LineGenRec(number);
+PrintResult(resultLine);
+
+
+
+
